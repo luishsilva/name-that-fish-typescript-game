60dd35fcdfabb56bedf772b4d93a8d3111a49e38
luishsilva <luizhrick@gmail.com>
2023-08-03 08:23:48 -0700

    Add handleIncorrectCount prop method and propType object

--- src/Components/Class/ClassGameBoard.tsx
+++ src/Components/Class/ClassGameBoard.tsx
@@ -1,6 +1,7 @@
 import { Component } from "react";
 import "./styles/game-board.css";
 import { Images } from "../../assets/Images";
+import  { FnType }  from "../../types"
 
 const initialFishes = [
   {
@@ -21,17 +22,41 @@ const initialFishes = [
   },
 ];
 
-export class ClassGameBoard extends Component {
+export class ClassGameBoard extends Component<FnType> {
+
+
+  handleFishGuessChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState(
+      {
+        fishName: e.target.value,
+        initialFishesNames: initialFishes,
+      }
+    );
+  }
+
+  handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    this.props.handleIncorrectCountCount();
+  }
+
+
   render() {
+    //const { fishName } = this.state;
+    
     const nextFishToName = initialFishes[0];
     return (
       <div id="game-board">
         <div id="fish-container">
           <img src={nextFishToName.url} alt={nextFishToName.name} />
         </div>
-        <form id="fish-guess-form">
+        <form onSubmit={this.handleSubmit} 
+          id="fish-guess-form">
           <label htmlFor="fish-guess">What kind of fish is this?</label>
-          <input type="text" name="fish-guess" />
+          <input 
+            type="text" 
+            name="fish-guess" 
+            onChange={this.handleFishGuessChange}
+          />
           <input type="submit" />
         </form>
       </div>
