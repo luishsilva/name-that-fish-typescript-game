64ac354bc8b19258ea0e0f26a8644e76efa55ce4
luishsilva <luizhrick@gmail.com>
2023-08-03 08:16:55 -0700

    Add counter and Fn type objects

--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,9 @@
 // If you have any types that are SHARED BY MANY COMPONENTS,put them here
+export type CounterType = {
+  incorrectCount: number,
+  correctCount: number,
+}
+
+export type FnType = {
+  handleIncorrectCountCount: () => void
+}
