c13d50586e76fed89f48226645fcc67fe48e0aff
luishsilva <luizhrick@gmail.com>
2023-08-03 08:20:01 -0700

    Add propstype Counter object and props to ClassScoreBoard and ClassGameBoard components

--- src/Components/Class/ClassApp.tsx
+++ src/Components/Class/ClassApp.tsx
@@ -2,18 +2,32 @@ import { Component } from "react";
 import { ClassScoreBoard } from "./ClassScoreBoard";
 import { ClassGameBoard } from "./ClassGameBoard";
 import { ClassFinalScore } from "./ClassFinalScore";
+import  { CounterType }  from "../../types"
 
 export class ClassApp extends Component {
-  state = {
+  state: CounterType = {
     incorrectCount: 0,
     correctCount: 0,
   };
+
+  handleIncorrectCountCount = () => {
+    this.setState({
+      incorrectCount: this.state.incorrectCount + 1
+    });
+  }
+
   render() {
+    const { incorrectCount, correctCount } = this.state
     return (
       <>
         <>
-          <ClassScoreBoard />
-          <ClassGameBoard />
+          <ClassScoreBoard 
+            incorrectCount={incorrectCount}
+            correctCount={correctCount}
+          />
+          <ClassGameBoard 
+            handleIncorrectCountCount={this.handleIncorrectCountCount}
+          />
         </>
         {false && <ClassFinalScore />}
       </>
