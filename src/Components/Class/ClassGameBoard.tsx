8f323bce1693d6ca5c48eb8ea64ff31f216e0c5f
luishsilva <luizhrick@gmail.com>
2023-08-11 08:15:56 -0700

    Add type to fishGuess state and GameBoardType, remove empty spaces from input and lowecase text

--- src/Components/Class/ClassGameBoard.tsx
+++ src/Components/Class/ClassGameBoard.tsx
@@ -1,65 +1,70 @@
-import { Component } from "react";
-import "./styles/game-board.css";
-import { Images } from "../../assets/Images";
-import  { FnType }  from "../../types"
+import { Component } from 'react';
+import './styles/game-board.css';
+import { initialFishes } from './fishesNames';
 
-const initialFishes = [
-  {
-    name: "trout",
-    url: Images.trout,
-  },
-  {
-    name: "salmon",
-    url: Images.salmon,
-  },
-  {
-    name: "tuna",
-    url: Images.tuna,
-  },
-  {
-    name: "shark",
-    url: Images.shark,
-  },
-];
+type FishGuessType = {
+    fishGuess: string;
+};
 
-export class ClassGameBoard extends Component<FnType> {
+export type GameBoardType = {
+    handleIncorrectCount: () => void;
+    handleCorrectCount: () => void;
+    nameFishes: Array<{ name: string; url: string }>;
+};
 
+export class ClassGameBoard extends Component<GameBoardType> {
+    state: FishGuessType = {
+        fishGuess: '',
+    };
 
-  handleFishGuessChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    this.setState(
-      {
-        fishName: e.target.value,
-        initialFishesNames: initialFishes,
-      }
-    );
-  }
+    handleFishGuessChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        this.setState({
+            fishGuess: e.target.value.toLocaleLowerCase().trim(),
+        });
+    };
 
-  handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    this.props.handleIncorrectCountCount();
-  }
+    handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const { nameFishes } = this.props;
 
+        const { fishGuess } = this.state;
 
-  render() {
-    //const { fishName } = this.state;
-    
-    const nextFishToName = initialFishes[0];
-    return (
-      <div id="game-board">
-        <div id="fish-container">
-          <img src={nextFishToName.url} alt={nextFishToName.name} />
-        </div>
-        <form onSubmit={this.handleSubmit} 
-          id="fish-guess-form">
-          <label htmlFor="fish-guess">What kind of fish is this?</label>
-          <input 
-            type="text" 
-            name="fish-guess" 
-            onChange={this.handleFishGuessChange}
-          />
-          <input type="submit" />
-        </form>
-      </div>
-    );
-  }
+        if (fishGuess) {
+            if (fishGuess !== nameFishes[0].name) {
+                this.props.handleIncorrectCount();
+            } else {
+                this.props.handleCorrectCount();
+            }
+        }
+
+        this.setState({ fishGuess: '' });
+    };
+
+    render() {
+        const { nameFishes } = this.props;
+        const nextFishToName = nameFishes[0] ? nameFishes[0] : initialFishes[3];
+        return (
+            <div id="game-board">
+                <div id="fish-container">
+                    <img src={nextFishToName.url} alt={nextFishToName.name} />
+                </div>
+                <form onSubmit={this.handleSubmit} id="fish-guess-form">
+                    <label htmlFor="fish-guess">
+                        What kind of fish is this?
+                    </label>
+                    <input
+                        type="text"
+                        id="fish-guess"
+                        name="fish-guess"
+                        value={this.state ? this.state.fishGuess : ''}
+                        onChange={this.handleFishGuessChange}
+                        ref = {(inp) => {
+                          inp?.focus();
+                       }}
+                    />
+                    <input type="submit" />
+                </form>
+            </div>
+        );
+    }
 }
