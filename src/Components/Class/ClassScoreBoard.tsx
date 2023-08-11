b57f21199440b314e25832410937820acbc05554
luishsilva <luizhrick@gmail.com>
2023-08-11 08:17:13 -0700

    Add type to ClassScoreBoard fix identation

--- src/Components/Class/ClassScoreBoard.tsx
+++ src/Components/Class/ClassScoreBoard.tsx
@@ -1,24 +1,22 @@
-import { Component } from "react";
-import "./styles/score-board.css";
+import { Component } from 'react';
+import './styles/score-board.css';
+import { ScoreBoardType } from '../../types';
 
-import  { CounterType }  from "../../types"
-
-const answersLeft = ["trout", "salmon", "tuna", "shark"];
-export class ClassScoreBoard extends Component<CounterType> {
-  render() {
-    const {incorrectCount, correctCount} = this.props;
-    return (
-      <div id="score-board">
-        <div>Incorrect 🔻: {incorrectCount}</div>
-        <div id="choices-left">
-          {answersLeft.map((answer) => (
-            <div key={answer} className="choice">
-              {answer}
+export class ClassScoreBoard extends Component<ScoreBoardType> {
+    render() {
+        const { incorrectCount, correctCount, nameFishes } = this.props;
+        return (
+            <div id="score-board">
+                <div>Incorrect 🔻: {incorrectCount}</div>
+                <div id="choices-left">
+                    {nameFishes.map((answer) => (
+                        <div key={answer.name} className="choice">
+                            {answer.name}
+                        </div>
+                    ))}
+                </div>
+                <div>Correct ✅: {correctCount}</div>
             </div>
-          ))}
-        </div>
-        <div>Correct ✅: {correctCount}</div>
-      </div>
-    );
-  }
+        );
+    }
 }
