d7296e3614eccd471453d78d1f841c2618fed9d1
luishsilva <luizhrick@gmail.com>
2023-08-15 08:49:13 -0700

    Display game components and final scor

--- src/Components/Functional/FunctionalApp.tsx
+++ src/Components/Functional/FunctionalApp.tsx
@@ -1,13 +1,45 @@
+import { useState } from 'react';
+
 import { FunctionalGameBoard } from './FunctionalGameBoard';
 import { FunctionalScoreBoard } from './FunctionalScoreBoard';
 import { FunctionalFinalScore } from './FunctionalFinalScore';
 
+import { initialFishes } from './fishesNames';
+
+type NameFishesType = {
+  name: string;
+  url: string;
+}[];
+
 export function FunctionalApp() {
+
+  const [incorrectCount, setIncorrectCount] = useState(0);
+  const [correctCount, setCorrectCount] = useState(0);
+  const [nameFishes, setNameFishes] = useState<NameFishesType>([...initialFishes]);
   return (
     <>
-      <FunctionalScoreBoard />
-      <FunctionalGameBoard />
-      {false && <FunctionalFinalScore />}
+    {nameFishes.length > 0 && (
+      <>
+        <FunctionalScoreBoard 
+          incorrectCount={incorrectCount}
+          correctCount={correctCount}
+          nameFishes={nameFishes}
+        />
+        <FunctionalGameBoard
+          incorrectCount={incorrectCount}
+          correctCount={correctCount}
+          nameFishes={nameFishes}
+          setIncorrectCount={setIncorrectCount}
+          setCorrectCount={setCorrectCount}
+        />
+      </>
+    )}
+    {nameFishes.length === 0 && 
+      <FunctionalFinalScore 
+        total={initialFishes.length}
+        correctCount={correctCount}
+      />
+    }
     </>
   );
 }
